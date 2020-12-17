5f9e7b503f95a1ad4e8d6d31bf4ad3fa78f8162c
Zeitheron Rowdan <zayika.dmytro@vu.cdu.edu.ua>
2020-12-17 10:58:20 +0200

    Added console input for numbers, output their sum

--- Program.cs
+++ Program.cs
@@ -6,7 +6,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.Write("x: ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("y: ");
+            int y = int.Parse(Console.ReadLine());
+
+            Console.WriteLine($"{x} + {y} = {x + y}");
             Console.ReadKey();
         }
     }
